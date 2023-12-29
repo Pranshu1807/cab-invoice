303476564025c7ff9326311c8fcd5a6418bbba88
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-30 00:49:51 +0530

    UC1 implemented

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,20 +1,23 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "5.0, 10.0, 60.0",
+            "10.2, 25.0, 127.0",
+            "1.5, 4.0, 19.0",
+            "0.2, 2.0, 5.0"
+    })
+    public void fareTest(double distance, double time, double expected) {
+        Ride ride = new Ride(distance, time);
+        double fare = ride.totalFare();
+        assertEquals(expected, fare, 0);
     }
+
 }
