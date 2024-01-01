9789412fe44a212ba6f7171a00853188ad900764
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2024-01-02 01:59:34 +0530

    UC1 implemented

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -12,7 +12,8 @@ public class AppTest {
             "5.0, 10.0, 60.0",
             "10.2, 25.0, 127.0",
             "1.5, 4.0, 19.0",
-            "0.2, 2.0, 5.0"
+            "0.2, 2.0, 5.0",
+            "5.6,15 ,71.0"
     })
     public void fareTest(double distance, double time, double expected) {
         Ride ride = new Ride(distance, time);
