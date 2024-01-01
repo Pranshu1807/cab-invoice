4016eea1e36f69eccbdc945d74323c89eb690fae
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2024-01-02 04:12:23 +0530

    UC4 implemented

--- src/main/java/com/example/CabInvoice.java
+++ src/main/java/com/example/CabInvoice.java
@@ -1,12 +1,24 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CabInvoice {
 
     public static double pricePerKm = 10.0, perPerMin = 1.0, minFare = 5.0;
+    public static HashMap<String, ArrayList<Ride>> rideRepo = new HashMap<>();
 
     public double calculateFare(Ride ride) {
         double totalFare = ride.distance * pricePerKm + ride.time * perPerMin;
         return Math.max(totalFare, minFare);
+    }
+
+    public void addRide(String user, double distance, double time) {
+        if (!rideRepo.containsKey(user)) {
+            rideRepo.put(user, new ArrayList<Ride>());
+        }
+        Ride ride = new Ride(distance, time);
+        rideRepo.get(user).add(ride);
     }
 
     public double total(Ride[] rides) {
@@ -26,6 +38,16 @@ public class CabInvoice {
     }
 
     public InvoiceReport getReport(Ride[] rides) {
+        return new InvoiceReport(total(rides), noOfRides(rides), avgFare(rides));
+    }
+
+    public InvoiceReport getReportByUser(String name) {
+        if (!rideRepo.containsKey(name)) {
+            return new InvoiceReport(0.0, 0, 0.0);
+        }
+        ArrayList<Ride> temp = rideRepo.get(name);
+        Ride[] rides = new Ride[temp.size()];
+        rides = temp.toArray(rides);
         return new InvoiceReport(total(rides), noOfRides(rides), avgFare(rides));
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -49,4 +49,33 @@ public class AppTest {
                 && Calculated.avgFare == Expected.avgFare);
     }
 
+    @Test
+    public void userInvoiceTest() {
+        CabInvoice cabInvoice = new CabInvoice();
+        cabInvoice.addRide("User1", 5.0, 10.0);
+        cabInvoice.addRide("User1", 4.0, 8.0);
+        cabInvoice.addRide("User2", 8.0, 20.0);
+        cabInvoice.addRide("User2", 0.3, 1.0);
+
+        InvoiceReport CalculatedUser1 = cabInvoice.getReportByUser("User1");
+        InvoiceReport ExpectedUser1 = new InvoiceReport(108.0, 2, 54.0);
+        assertEquals(true,
+                CalculatedUser1.totalFare == ExpectedUser1.totalFare
+                        && CalculatedUser1.noOfRides == ExpectedUser1.noOfRides
+                        && CalculatedUser1.avgFare == ExpectedUser1.avgFare);
+
+        InvoiceReport CalculatedUser2 = cabInvoice.getReportByUser("User2");
+        InvoiceReport ExpectedUser2 = new InvoiceReport(105.0, 2, 52.5);
+        assertEquals(true,
+                CalculatedUser2.totalFare == ExpectedUser2.totalFare
+                        && CalculatedUser2.noOfRides == ExpectedUser2.noOfRides
+                        && CalculatedUser2.avgFare == ExpectedUser2.avgFare);
+        InvoiceReport CalculatedUser3 = cabInvoice.getReportByUser("User3");
+        InvoiceReport ExpectedUser3 = new InvoiceReport(0.0, 0, 0.0);
+        assertEquals(true,
+                CalculatedUser3.totalFare == ExpectedUser3.totalFare
+                        && CalculatedUser3.noOfRides == ExpectedUser3.noOfRides
+                        && CalculatedUser3.avgFare == ExpectedUser3.avgFare);
+    }
+
 }
